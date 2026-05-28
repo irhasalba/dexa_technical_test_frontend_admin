5d6e5672a4a5e6c54721c527e90fa8768eb86acf
irhasalbaisfit <irhas@fit.id>
2026-05-28 10:47:53 +0700

    add env

--- src/config/axios.tsx
+++ src/config/axios.tsx
@@ -1,7 +1,7 @@
 import axios from "axios"
 
 const baseUrl = axios.create({
-    baseURL : "http://localhost:3000/api/v1/"
+    baseURL : import.meta.env.VITE_API_BASE_URL
 })
 
 baseUrl.interceptors.request.use((config) => {
--- src/config/firebase.ts
+++ src/config/firebase.ts
@@ -2,15 +2,15 @@ import { initializeApp } from "firebase/app";
 import { getMessaging } from "firebase/messaging";
 
 const firebaseConfig = {
-  apiKey: "AIzaSyCfUrRDMHIimSV1lsv4LAeBxeQFVWr7Voo",
-  authDomain: "dexa-hr-apps.firebaseapp.com",
-  projectId: "dexa-hr-apps",
-  storageBucket: "dexa-hr-apps.firebasestorage.app",
-  messagingSenderId: "590724636836",
-  appId: "1:590724636836:web:50be93b96d99450f094631",
+  apiKey: import.meta.env.VITE_FIREBASE_API_KEY,
+  authDomain: import.meta.env.VITE_FIREBASE_AUTH_DOMAIN,
+  projectId: import.meta.env.VITE_FIREBASE_PROJECT_ID,
+  storageBucket: import.meta.env.VITE_FIREBASE_STORAGE_BUCKET,
+  messagingSenderId: import.meta.env.VITE_FIREBASE_MESSAGING_SENDER_ID,
+  appId: import.meta.env.VITE_FIREBASE_APP_ID,
 };
 
-export const VAPID_KEY = "BCmPBZYV8dNhTuuUtESccLF1WlwQdulKcMYVuVnEtgXQhYI3DkQadSwmnMiJCD09_6dBA1x9nx3L1V-gj5HngCc";
+export const VAPID_KEY = import.meta.env.VITE_FIREBASE_VAPID_KEY;
 
 export const FCM_TOPIC = "update_data"
 
